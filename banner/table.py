c82a6acdea2f15d5416283bced969ec3de9c1e9c
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-21 20:25:55 +0530

    Create table.py

---
+++ banner/table.py
@@ -0,0 +1,15 @@
+#Set color
+R = '\033[31m' # Red
+N = '\033[1;37m' # White
+G = '\033[32m' # Green
+O = '\033[0;33m' # Orange
+B = '\033[1;34m' #Blue
+print 
+print " com "
+print " Exploit "
+print " Exploit "
+print " Exploit "
+print " Exploit "
+print " Exploit "
+print " Exploit "
+print 
