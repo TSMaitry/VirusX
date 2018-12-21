42a7b01281ac8de727dfa28ab390a16fe6646c5a
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-21 15:30:01 +0530

    Update virus-x.py

--- virus-x.py
+++ virus-x.py
@@ -12,7 +12,32 @@ G = '\033[32m' # Green
 O = '\033[0;33m' # Orange
 B = '\033[1;34m' #Blue
 os.system("cd banner;python2 banner4.py")
-
 print
 print ("\033[44m                     Welcome To Virus-X                     \033[0m")
-print 
+print ("\033[44m                     Author: Trilok Singh                   \033[0m")
+print
+
+def help():
+  print
+  print ("\033[44m Hi \033[0m")
+  print ("\033[44m Hello\033[0m")
+  print ("\033[44m Hi\033[0m")
+  print
+  main()
+
+def main():
+  dr = raw_input(">>> ")
+  if dr == "show options":
+    time.sleep(1)
+    help()
+    main()
+  elif dr == "virus-x update":
+    print ""+G+"~ "+B+"UPDATING..."
+    time.sleep(2)
+    print ""+G+"Cloning repo..."
+    time.sleep(3)
+    os.system("cd ..;rm -rf virus-x;git clone https://github.com/TSMaitry/Virus-X;cd virus-x;python2 virus-x.py")
+    main()
+if __name__ == "__main__":
+	main()
+  
