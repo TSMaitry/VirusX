eafb4b928befb1ca0fd325dacc08dc64deabb898
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-22 00:30:57 +0530

    Update table.py

--- banner/table.py
+++ banner/table.py
@@ -9,5 +9,17 @@ def force_to_unicode(text):
   "If text is unicode, it is returned as is. If it's str, convert it to Unicode using UTF-8 encoding" 
   return text if isinstance(text, unicode) else text.decode('utf8')
 print 
-print "┐ "
+print "▛▀▀▀▀▀▀▜"
+print "▌1        ▐"
+print "▌2        ▐"
+print "▌3        ▐"
+print "▌4        ▐"
+print "▌5        ▐"
+print "▌6        ▐"
+print "▌7        ▐"
+print "▌8        ▐"
+print "▌9        ▐"
+print "▌10       ▐"
+print "▌exit     ▐"
+print "▙▄▄▄▄▄▄▟"
 print 
