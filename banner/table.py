09f1e1d3bbcf29cab4274b9136302bb085e2d28f
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-22 00:18:07 +0530

    Update and rename table to table.py

---
+++ banner/table.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+#Set color
+R = '\033[31m' # Red
+N = '\033[1;37m' # White
+G = '\033[32m' # Green
+O = '\033[0;33m' # Orange
+B = '\033[1;34m' #Blue
+def force_to_unicode(text): 
+  "If text is unicode, it is returned as is. If it's str, convert it to Unicode using UTF-8 encoding" 
+  return text if isinstance(text, unicode) else text.decode('utf8')
+print 
+print "┐ "
+print 
