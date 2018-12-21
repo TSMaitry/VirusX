fdc291c725f9760b50e6f4ba033cf696d67c28d3
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-21 15:04:02 +0530

    Add files via upload

---
+++ virus-x.py
@@ -0,0 +1,18 @@
+#!/usr/bin/evn python
+
+import sys
+import os
+import time
+import os as sistema
+
+# Set color
+R = '\033[31m' # Red
+N = '\033[1;37m' # White
+G = '\033[32m' # Green
+O = '\033[0;33m' # Orange
+B = '\033[1;34m' #Blue
+os.system("cd banner;python2 banner4.py")
+
+print
+print ("\033[44m                     Welcome To Virus-X                     \033[0m")
+print 
