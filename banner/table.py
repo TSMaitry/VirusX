e5d4d03b6b475ce6bd362d076e4f90ecdea50710
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-21 22:03:05 +0530

    Update table.py

--- banner/table.py
+++ banner/table.py
@@ -4,19 +4,5 @@ N = '\033[1;37m' # White
 G = '\033[32m' # Green
 O = '\033[0;33m' # Orange
 B = '\033[1;34m' #Blue
-print 
-print ('┏━━━━━━━━━━━━━━━━┓')
-print ('┃ 1. Virus                ┃')
-print ("┃ 2. Virus                ┃")
-print ("┃ 3. Virus                ┃")
-print ("┃ 4. Virus                ┃")
-print ("┃ 5. Virus                ┃")
-print ("┃ 6. Virus                ┃")
-print ("┃ 7. Virus                ┃")
-print ("┃ 8. Virus                ┃")
-print ("┃ 9. Virus                ┃")
-print ("┃ 10. Virus               ┃")
-print ("┃ exit                    ┃")
-print ("┃                         ┃")
-print ("┗━━━━━━━━━━━━━━━━┛")
+prin
 print 
