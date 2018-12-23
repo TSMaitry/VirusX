3edb5e042de0f272351e8202b396d01d9675d8cc
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-24 02:42:22 +0530

    Update table.py

--- banner/table.py
+++ banner/table.py
@@ -11,16 +11,18 @@ def force_to_unicode(text):
   "If text is unicode, it is returned as is. If it's str, convert it to Unicode using UTF-8 encoding" 
   return text if isinstance(text, unicode) else text.decode('utf8')
 
-print 
-print ("00. Android Bootlop\n")
-print ("01. Android Freeze\n")
-print ("02. Android Rans\n")
-print ("03. Android Malware\n")
-print ("04. Android Decryptor\n")
-print ("05. Android Decryptor 2\n")
-print ("06. Android SMS Tief\n")
-print ("07. Android Dangerous Malware\n")
-print ("08. Android ENC Malware\n")
-print ("09. Android Door Trojan\n")
-print ("10. "+R+"EXIT\n")
-print 
+print
+print ("┏━━━━━━━━━━━━━━━━━━━━━━┓")
+print ("┃00.        Android Bootlop        ┃")
+print ("┃01.        Android Freezen        ┃")
+print ("┃02.         Android Ransn         ┃")
+print ("┃03.        Android Malware        ┃")
+print ("┃04.       Android Decryptor       ┃")
+print ("┃05.       Android Decryptor2      ┃")
+print ("┃06.        Android SMS Tief       ┃")
+print ("┃07.   Android Dangerous Malware   ┃")
+print ("┃08.      Android ENC Malware      ┃")
+print ("┃09.      Android Door Trojan      ┃")
+print ("┃10.         "+R+"EXIT             ┃")
+print ("┗━━━━━━━━━━━━━━━━━━━━━━┛")
+print
